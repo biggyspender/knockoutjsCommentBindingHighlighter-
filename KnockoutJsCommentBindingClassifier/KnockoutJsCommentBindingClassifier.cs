0857db680d26fa6011e5171a16f6b849b21720f7
biggyspender <chris@radiotuna.com>
2015-03-06 16:29:43 +0000

    improved snapshot cacheing

--- KnockoutJsCommentBindingClassifier/KnockoutJsCommentBindingClassifier.cs
+++ KnockoutJsCommentBindingClassifier/KnockoutJsCommentBindingClassifier.cs
@@ -2,9 +2,10 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text.RegularExpressions;
-using System.Windows.Media;
 using HtmlAgilityPack;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
@@ -14,11 +15,12 @@ namespace KnockoutJsCommentBindingClassifier
 {
 
     #region Provider definition
+
     /// <summary>
     /// This class causes a classifier to be added to the set of classifiers. Since 
     /// the content type is set to "text", this classifier applies to all text files
     /// </summary>
-    [Export(typeof(IClassifierProvider))]
+    [Export(typeof (IClassifierProvider))]
     [ContentType("HTML")]
     internal class KnockoutJsCommentBindingClassifierProvider : IClassifierProvider
     {
@@ -26,30 +28,35 @@ namespace KnockoutJsCommentBindingClassifier
         /// Import the classification registry to be used for getting a reference
         /// to the custom classification type later.
         /// </summary>
-        [Import]
-        internal IClassificationTypeRegistryService ClassificationRegistry = null; // Set via MEF
+        [Import] internal IClassificationTypeRegistryService ClassificationRegistry = null; // Set via MEF
 
         public IClassifier GetClassifier(ITextBuffer buffer)
         {
-            return buffer.Properties.GetOrCreateSingletonProperty<KnockoutJsCommentBindingClassifier>(delegate { return new KnockoutJsCommentBindingClassifier(ClassificationRegistry); });
+            return buffer
+                .Properties
+                .GetOrCreateSingletonProperty<KnockoutJsCommentBindingClassifier>(
+                    () => new KnockoutJsCommentBindingClassifier(ClassificationRegistry));
         }
     }
+
     #endregion //provider def
 
     #region Classifier
+
     /// <summary>
     /// Classifier that classifies all text as an instance of the OrinaryClassifierType
     /// </summary>
-    class KnockoutJsCommentBindingClassifier : IClassifier
+    internal class KnockoutJsCommentBindingClassifier : IClassifier
     {
-        IClassificationType _classificationType;
-
+        private readonly IClassificationType classificationType;
+        private readonly Stopwatch stopwatch;
         internal KnockoutJsCommentBindingClassifier(IClassificationTypeRegistryService registry)
         {
-            _classificationType = registry.GetClassificationType("KnockoutJsCommentBindingClassifier");
+            classificationType = registry.GetClassificationType("KnockoutJsCommentBindingClassifier");
+            stopwatch = Stopwatch.StartNew();
         }
 
-        private ConcurrentDictionary<ITextSnapshot,IEnumerable<Span>> snapshotLookups=new ConcurrentDictionary<ITextSnapshot, IEnumerable<Span>>(); 
+        private readonly MemoryCache memoryCache = new MemoryCache(Guid.NewGuid().ToString("N"));
 
         /// <summary>
         /// This method scans the given SnapshotSpan for potential matches for this classification.
@@ -60,22 +67,27 @@ namespace KnockoutJsCommentBindingClassifier
         public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
         {
             var textSnapshot = span.Snapshot;
-
-            var knockoutNodePositions = snapshotLookups.GetOrAdd(textSnapshot, KnockoutNodePositions);
+            var text = textSnapshot.GetText();
+            var knockoutNodePositions = GetKnockoutNodePositions(text);
 
             var classificationSpans = knockoutNodePositions.Where(
-                np =>  np.Start >= span.Start.Position && np.Start < span.End.Position)
+                np => np.Start >= span.Start.Position && np.Start < span.End.Position)
                 .Select(
                     knp =>
                         new ClassificationSpan(new SnapshotSpan(textSnapshot, new Span(knp.Start, knp.Length)),
-                            _classificationType))
+                            classificationType))
                 .ToList();
             return classificationSpans;
         }
 
-        private static IEnumerable<Span> KnockoutNodePositions(ITextSnapshot textSnapshot)
+        private IEnumerable<Span> GetKnockoutNodePositions(string text)
         {
-            var text = textSnapshot.GetText();
+            return memoryCache.GetOrStore(text, () => KnockoutNodePositions(text), 10);
+        }
+
+      
+        private static IEnumerable<Span> KnockoutNodePositions(string text)
+        {
             var doc = new HtmlDocument();
             doc.LoadHtml(text);
             var knockoutNodes =
@@ -97,5 +109,6 @@ namespace KnockoutJsCommentBindingClassifier
         public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
 #pragma warning restore 67
     }
+
     #endregion //Classifier
 }
